c24a9f3ea2d4241e10b30dccc7210caddf5b8326
Ruslan <khusenov96@list.ru>
2023-06-19 22:33:46 +0600

    feat: anal

--- Riot/Modules/Common/Recents/Views/AdSheetView.swift
+++ Riot/Modules/Common/Recents/Views/AdSheetView.swift
@@ -37,7 +37,7 @@ struct AdSheetView: View{
             case .phoneNumber:
             return "tel:\(clientAd.phoneNumber ?? "")"
             case .whatsApp:
-            return "https://api.whatsapp.com/send?phone=\(clientAd.phoneNumber ?? "")&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D0%B8%D0%B7%20BigStar%20Messenger"
+            return "https://api.whatsapp.com/send?phone=\(clientAd.phoneNumber ?? "")&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D0%B8%D0%B7%20BigStar%20Messenger%20https://bigstar.netlify.app/"
         }
     }
 
